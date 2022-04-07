fbc46558af21798ed4e4b0c912e27f25609c1952
nayakrujul <nayakrujul@hotmail.com>
2022-04-07 18:25:55 +0100

    Minor change for v1.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 from pathlib import Path
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = open('README.md').read()
 
 setup(
   name = 'badsql',
   packages = ['badsql'],
-  version = '1.0.1',
+  version = '1.0.2',
   license='MIT',
   description = 'SQL but worse',
   author = 'Rujul Nayak',
