83bf0603e2f1182f6ec68c28986b954886cf8e84
First Name Last Name <55329600+nayakrujul@users.noreply.github.com>
2022-04-07 15:02:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
-
+from distutils.core import setup
+setup(
+  name = 'badsql',
+  packages = ['badsql'],
+  version = '0.1',
+  license='MIT',
+  description = 'SQL but worse',
+  author = 'Rujul Nayak',
+  author_email = 'rujulnayak@outlook.com',
+  url = 'https://github.com/nayakrujul/badsql',
+  download_url = 'https://github.com/nayakrujul/badsql/archive/refs/tags/v_01.tar.gz',
+  keywords = ['database', 'SQL', 'worse'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
